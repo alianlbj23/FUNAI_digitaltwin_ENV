e75a2f88fada0a6f237b6765d189340091462c4b
alianlbj23 <alianlbj23@gmail.com>
2024-03-07 12:37:14 +0800

    Implement automatic IP retrieval
    Develop functionality to automatically detect the IP address of the current computer,
    prioritizing the WiFi interface for ROSBridge connection.
    
    If the WiFi interface IP address is not found,
    the Ethernet interface IP address will be searched for.
    
    If neither interface is available,
    the localhost address will be returned.
    
    This automation streamlines the process of connecting Unity to ROSBridge by eliminating the need for manual IP input

--- Assets/Scripts/ConnectROS/IpManager.cs
+++ Assets/Scripts/ConnectROS/IpManager.cs
@@ -1,20 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Linq;
 
 public class IpManager : MonoBehaviour
 {
-    public string ip = "192.168.68.110";
+    // private string ip = "192.168.68.110";
     private string protocol = "ws://";
     private string port = "9090";
     public string FullUrl
     {
-        get { return $"{protocol}{ip}:{port}"; }
+        get { return $"{protocol}{GetIpAddress()}:{port}"; }
     }
-    public void SetIp(string newIp)
+    private string GetIpAddress()
     {
-        ip = newIp;
+        // 優先尋找 Wi-Fi 介面卡的 IP 地址
+        string ip = GetIpByNetworkInterfaceType(NetworkInterfaceType.Wireless80211);
+        if (!string.IsNullOrEmpty(ip))
+        {
+            return ip;
+        }
+
+        // 如果找不到 Wi-Fi IP，則尋找乙太網路卡的 IP 地址
+        ip = GetIpByNetworkInterfaceType(NetworkInterfaceType.Ethernet);
+        if (!string.IsNullOrEmpty(ip))
+        {
+            return ip;
+        }
+
+        // 如果兩者都找不到，返回預設 IP 或錯誤訊息
+        return "127.0.0.1"; // 或返回適當的錯誤訊息
     }
+    private string GetIpByNetworkInterfaceType(NetworkInterfaceType type)
+    {
+        foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+        {
+            if (ni.NetworkInterfaceType == type && ni.OperationalStatus == OperationalStatus.Up)
+            {
+                IPInterfaceProperties properties = ni.GetIPProperties();
+                foreach (UnicastIPAddressInformation ip in properties.UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        return ip.Address.ToString();
+                    }
+                }
+            }
+        }
+
+        return string.Empty; // 沒有找到符合條件的 IP 地址
+    }
+    // public void SetIp(string newIp)
+    // {
+    //     ip = newIp;
+    // }
     public string GetIp()
     {
         return FullUrl;
